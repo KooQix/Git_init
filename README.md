ae9087e52d63e4b1b581c02698b1de323dfae4bf
KooQix <legoutpaul@gmail.com>
2021-12-28 13:03:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### A simple shell script to initialize a git remote repository and create a global .gitignore.
+
+## Initialization
+
+    sudo chmod 700 git_init.sh
+
+## Usage
+
+    sudo ./git_init.sh git@github.com:user/repo.git
+
+# Feel free to modify it to your preferences
---
+++ git_init.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+
+# Param looks like git@github.com:user/test.git
+
+
+if [ "$#" -ne 1 ]
+then
+	echo "Error: Takes git repository as parameter"
+	exit
+fi
+
+
+# Check if the parameter is a git repo
+repo=`echo "$1" | tr '.' '\n' | head -1`
+suffix=`echo "$1" | tr '.' '\n' | tail -1`
+
+if [ "$repo" != "git@github" ] || [ "$repo" != "git@gitlab" ] && [ "$suffix" != "git" ]
+then
+	echo "Error: Parameter must be a git repository"
+	exit
+fi
+
+
+
+
+	#################### Repository is checked, start init ####################
+
+
+git init
+echo "
+**/.gitignore
+**/.env
+**/nodes_modules
+**/Dockerfile
+**/.dockerignore
+**/__pycache__/
+
+
+**/vendor
+
+## IDEs
+.vscode/
+*.pydevproject
+.project
+.metadata
+*.swp
+*~.nib
+local.properties
+.classpath
+.loadpath
+.externalToolBuilders/
+.buildpath
+*.launch
+.settings/
+.cproject
+.buildpath
+
+## OSes
+[Tt]humbs.db
+[Dd]esktop.ini
+*.DS_store
+
+
+## Misc
+**/bin/
+**/tmp/
+*.tmp
+*.bak
+*.log
+*.[Cc]ache
+*.cpr
+*.orig
+*.php.in
+.idea/
+temp/
+.Trashes
+.svn
+*.codekit
+*.scssc
+._*
+
+## Distribute
+**/dist/
+**/bundle/
+
+" > .gitignore
+
+
+
+git add -A
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin $1
+git push -u origin main
